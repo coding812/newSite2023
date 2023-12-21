b183beaf5602e30fa56d3c4a46797a692d4e282a
unknown <coding812@gmail.com>
2023-12-21 16:59:21 -0500

    added styling to navbar, improved responsiveness overall, added info in about page

--- src/Shared/NavBar.js
+++ src/Shared/NavBar.js
@@ -1,9 +1,7 @@
 import '../Shared/NavBar.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-// import Nav from 'react-bootstrap/Nav';
 import { NavLink } from 'react-router-dom';
-
 import CodeSymbol from '../Shared/Media/code.png';
 
 function NavBar() 
@@ -12,15 +10,11 @@ function NavBar()
     <Container className='navbar'>
       <Navbar expand="lg">
         <Container>
-          {/* <NavLink to="/home" className="nav-link">Home</NavLink>
-          <NavLink to="/about" className="nav-link">About</NavLink> */}
-          <NavLink to="/" className="Active link">Home</NavLink>
-          <NavLink to="/about" className="active link">About</NavLink>
+          <NavLink to="/" className="link">Home</NavLink>
+          <NavLink to="/about" className="link">About</NavLink>
           <img src={CodeSymbol} className='codeSymbol' alt='Self closing HTML Tag' />
-          {/* <NavLink to="/portfolio" className="nav-link">Portfolio</NavLink>
-          <NavLink to="/contact" className="nav-link">Contact</NavLink> */}
-          <NavLink to="/contact" className="active link">Contact</NavLink>
-          <NavLink to="/portfolio" className="active link">Work</NavLink>
+          <NavLink to="/contact" className="link">Contact</NavLink>
+          <NavLink to="/portfolio" className="link">Work</NavLink>
         </Container>
       </Navbar>
     </Container>
